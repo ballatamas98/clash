587ce4b0aa2aa25e1e7bf7c03989948a41555a63
ballatamas98 <36437827+ballatamas98@users.noreply.github.com>
2023-02-21 11:43:48 +0100

    bug fix

--- Arena.ts
+++ Arena.ts
@@ -4,30 +4,33 @@ import { Weapon } from './weapons/Weapon';
 export class Arena {
     public static tournament(hero1: Hero, hero2: Hero) {
         let round: number = 1;
-        while (hero1.getHealthPoints() > 0 || hero2.getHealthPoints() > 0) {
+        while (hero1.getHealthPoints() > 0 && hero2.getHealthPoints() > 0) {
+            console.log("------------------------------------------------");
             console.log("Round: " + round + "\n");
+            
             ({ hero1, hero2 } = Arena.suffleHeros(hero1, hero2));
-            let hasHero1AbilityActivated: boolean = this.heroAbility(hero1);
-            let hasHero2AbilityActivated: boolean = this.heroAbility(hero2);
+            let hasHero1AbilityActivated: boolean = this.isHeroAbilityActivated(hero1);
+            let hasHero2AbilityActivated: boolean = this.isHeroAbilityActivated(hero2);
 
             console.log(hero1.status() + " -> " + hero2.status());
+
+
             this.attack(hero1, hero2);
             if (hero2.getHealthPoints() <= 0) {
                 this.removeHeroAbilityEffects(hasHero1AbilityActivated, hero1, hasHero2AbilityActivated, hero2);
                 break;
             }
             console.log();
+
             console.log(hero2.status() + " -> " + hero1.status());
             this.attack(hero2, hero1);
             console.log();
 
             this.removeHeroAbilityEffects(hasHero1AbilityActivated, hero1, hasHero2AbilityActivated, hero2);
-
             round++;
         }
 
         console.log("%cThe winner is: " + (hero1.getHealthPoints() > 0 ? hero1.getName() : hero2.getName()), "color: gold");
-
     }
 
     private static removeHeroAbilityEffects(hasHero1AbilityActivated: boolean, hero1: Hero, hasHero2AbilityActivated: boolean, hero2: Hero) {
@@ -62,7 +65,7 @@ export class Arena {
                 return;
             }
             console.log("initialDamage: " + initialDamage.toFixed(2));
-            console.log(defender.getName() + " has sufferd: " + defender.sufferDamege(initialDamage).toFixed(2) + " damage");
+            console.log("%c" + defender.getName() + " has sufferd: " + defender.sufferDamege(initialDamage).toFixed(2) + " damage", "color: red");
 
         } else {
             console.log(attaker.getName() + " can't use the weapon: " + attaker.getWeapon().getName());
@@ -71,7 +74,7 @@ export class Arena {
 
     }
 
-    private static heroAbility(hero: Hero): boolean {
+    private static isHeroAbilityActivated(hero: Hero): boolean {
         if (Math.random() < 0.1) {
             hero.getAbility().doEffect(hero);
             console.log("%c" + hero.getName() + "'s ability has been activated", "color: yellow");
